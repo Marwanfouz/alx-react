af120bd6d9a683c0bd256fc68c790d61d426bdcb
Marwan Fouz <marwanfouz76@gmail.com>
2024-02-06 17:43:07 +0200

    5. Create basic tests with four tests

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,39 @@
+import { getFullYear, getFooterCopy } from './utils'
+import logo from './holberton_logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <section className="body-form-input">
+          <section className="email-input">
+          <label>
+            Email:
+            <input type="email" name="email" id="email" />
+          </label>
+          </section>
+          <section className="password-input">
+          <label>
+            Password:
+            <input type="password" name="password" id="password" />
+          </label>
+          </section>
+          <button>OK</button>
+        </section>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
