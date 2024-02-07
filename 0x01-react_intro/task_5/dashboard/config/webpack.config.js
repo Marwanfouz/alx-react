53a1d2f04862d3e69d5a184579e07f9867d13f41
Marwan Fouz <marwanfouz76@gmail.com>
2024-02-07 19:17:09 +0200

    11. Reorganize the files & tests

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,43 +1,54 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'bundle.js',
+    filename: "bundle.js",
   },
-  devServer: {
-    contentBase: path.join(__dirname, '../dist'),
-    port: 3000,
-    hot: true,
-  },
+  mode: "development",
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        // type: 'asset/resource',
         use: [
-            'file-loader',
-            {
-              loader: "image-webpack-loader",
-              options: {
-                bypassOnDebug: true,
-                disable: true,
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
             },
           },
         ],
       },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
     ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    static: "./dist",
+    compress: true,
+    open: true,
+    hot: true,
+    port: 8564,
   },
   devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      name: "index.html",
+      inject: false,
+      template: "./dist/index.html",
     }),
   ],
 };
