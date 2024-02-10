b5cab7efc5dd506934ceb1a1339bc4750f5186f1
Marwan Fouz <marwanfouz76@gmail.com>
2024-02-10 04:31:44 +0200

    init task

---
+++ 0x03-React_component/task_0/dashboard/src/Header/Header.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./Header";
+
+describe("<Header />", () => {
+  it("renders without crashing", () => {
+    const Component = shallow(<Header />);
+
+    expect(Component.exists()).toEqual(true);
+  });
+  it("render img and h1 tags", () => {
+    const Component = shallow(<Header />);
+
+    expect(Component.exists("img")).toEqual(true);
+    expect(Component.containsMatchingElement(<h1>School dashboard</h1>)).toEqual(true);
+  });
+});
