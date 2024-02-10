cb6829cad9b7e9f3c2a1ec14111d8b5b134c41ec
Marwan Fouz <marwanfouz76@gmail.com>
2024-02-10 04:27:43 +0200

    5. Shapes, Loops, and Keys

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -5,15 +5,28 @@ import Login from "../Login/Login";
 import Footer from "../Footer/Footer";
 import CourseList from "../CourseList/CourseList";
 import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
 import "./App.css";
 
 function App({ isLoggedIn }) {
+  const listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+
+  const listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
+
   return (
     <>
-      <Notifications/>
+      <Notifications listNotifications={listNotifications}/>
       <div className="App">
       <Header />
-      {isLoggedIn ? <CourseList /> : <Login />}
+      {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
       <Footer />
       </div>
     </>
--- 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Notifications.css";
 import closeIcon from "../assets/close-icon.png";
-import { getLatestNotification } from "../utils/utils";
 import NotificationItem from "./NotificationItem";
 import PropTypes from "prop-types";
+import NotificationItemShape from "./NotificationItemShape";
 
-function Notifications({ displayDrawer }) {
+function Notifications({ displayDrawer, listNotifications }) {
   return (
     <>
     {displayDrawer ? (
@@ -23,10 +23,11 @@ function Notifications({ displayDrawer }) {
         <img src={closeIcon} alt="closeIcon" width="10px" />
       </button>
       <p>Here is the list of notifications</p>
-      <ul>
-        <NotificationItem type="default" value="New course available"/>
-        <NotificationItem type="urgent" value="New resume available" />
-        <NotificationItem type="urgent" html={getLatestNotification()} />
+      <ul>{listNotifications ? (
+        listNotifications.map(({id, html, type, value}) => <NotificationItem key={id} type={type} value={value} html={html} />)
+      ) : (
+        <NotificationItem value="No new notification for now" />
+      )}
       </ul>
       </div>
     </div>
@@ -42,6 +43,7 @@ function Notifications({ displayDrawer }) {
 
 Notifications.propTypes = {
   displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
 };
 
 Notifications.defaultProps = {
