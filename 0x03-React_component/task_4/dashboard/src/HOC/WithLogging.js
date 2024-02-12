eb17eb54e833bc8caa5efa61a8a60e2af9ce4076
Marwan Fouz <marwanfouz76@gmail.com>
2024-02-12 22:13:21 +0200

    8. Write a test for the HOC

--- 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.js
+++ 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.js
@@ -2,15 +2,17 @@ import React from "react";
 
 const WithLogging = ({ WrappedComponent }) => {
 
-  const displayName = (WrappedComponent) => WrappedComponent.displayName || WrappedComponent.name || "Component";
+  const getDisplayName = (WrappedComponent) => WrappedComponent.displayName || WrappedComponent.name || "Component";
+
+  WithLogging.displayName = `WithLogging(${getDisplayName(WrappedComponent)})`;
 
   return class extends React.Component {
     componentDidMount() {
-      console.log(`Component ${displayName(WrappedComponent)} is mounted`);
+      console.log(`Component ${getDisplayName(WrappedComponent)} is mounted`);
     }
   
     componentWillUnmount() {
-      console.log(`Component ${displayName(WrappedComponent)} is going to unmount`);
+      console.log(`Component ${getDisplayName(WrappedComponent)} is going to unmount`);
     }
     
     render() {
